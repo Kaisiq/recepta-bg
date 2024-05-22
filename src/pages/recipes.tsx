0e35e952a376715feffd6755eaee81ac9d7ff53d
kaisiq <dahudohu@gmail.com>
2024-05-22 18:21:49 +0300

    prettier

--- src/pages/recipes.tsx
+++ src/pages/recipes.tsx
@@ -6,20 +6,26 @@ const RecipesPage = () => {
 
   return (
     <div>
-      {recipes.map((recipe) => (
-        <>
-          <div key={recipe.id}>
-            <h2>{recipe.name}</h2>
-            <p>{recipe.description}</p>
-            <p>Time: {recipe.time} minutes</p>
-            <p>Products: {recipe.products.join(', ')}</p>
-            <img src={recipe.photo} alt={recipe.name} />
-            <p>{recipe.details}</p>
-            <p>Tags: {recipe.tags.join(', ')}</p>
-            <p>Last Modified: {recipe.updatedAt.toLocaleDateString()}</p>
-          </div>
-        </>
-      ))}
+      {recipes.map((recipe) => {
+        const date = new Date(recipe.updatedAt);
+        return (
+          <>
+            <div key={recipe.id}>
+              <h2>{recipe.name}</h2>
+              <p>{recipe.description}</p>
+              <p>Time: {recipe.time} minutes</p>
+              <p>Products: {recipe.products.join(", ")}</p>
+              <img
+                src={recipe.photo}
+                alt={recipe.name}
+              />
+              <p>{recipe.details}</p>
+              <p>Tags: {recipe.tags.join(", ")}</p>
+              <p>Last Modified: {date.toLocaleDateString()}</p>
+            </div>
+          </>
+        );
+      })}
     </div>
   );
 };
